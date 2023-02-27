936450d01b0fbf7beeb0ed6b1db44a4210fa8eb8
AliRiza <imrealiriza11@gmail.com>
2023-02-27 22:18:28 +0100

    Team2 Class ist geändert!

--- src/GrupCalismasi/Team2.java
+++ src/GrupCalismasi/Team2.java
@@ -1,4 +1,8 @@
 package GrupCalismasi;
 
 public class Team2 {
+    public static void main(String[] args) {
+        System.out.println("Team2 macht am Bestens miteinander!");
+    }
+    // Team2 macht am Bestens miteinander!
 }
