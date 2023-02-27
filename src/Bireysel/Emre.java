5a384917aa9504401ee4e38693d446ba7f01127d
e1982b <e1982b@gmail.com>
2023-02-27 21:28:50 +0100

    File Uploaded

--- src/Bireysel/Emre.java
+++ src/Bireysel/Emre.java
@@ -3,5 +3,6 @@ package Bireysel;
 public class Emre {
     public static void main(String[] args) {
         System.out.println("Created by Emre");
+        System.out.println("Test in Local");
     }
 }
