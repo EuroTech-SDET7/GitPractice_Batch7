e4d8eed7b6a986f06c7965feb8eda254149fe5f2
Talha <talha43gs@gmail.com>
2023-02-27 20:03:04 +0000

    Base project Setup

---
+++ src/Bireysel/Talha.java
@@ -0,0 +1,9 @@
+package Bireysel;
+
+public class Talha {
+
+
+    public static void main(String[] args) {
+        System.out.println("TEST_LOCAL");
+    }
+}
---
+++ src/OrtakDosyalar/Test.java
@@ -0,0 +1,4 @@
+package OrtakDosyalar;
+
+public class Test {
+}
