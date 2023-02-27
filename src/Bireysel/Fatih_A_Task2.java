48280ba43d3620ba16a418ff26ae38da2f3e568c
Fatih <faydan80@outlook.com>
2023-02-27 22:40:13 +0100

    The first step of the second task is accomplished.

---
+++ src/Bireysel/Fatih_A_Task2.java
@@ -0,0 +1,20 @@
+package Bireysel;
+
+public class Fatih_A_Task2 {
+
+    public static void main(String[] args) {
+        System.out.println("Lets practice Method Overloading");
+    }
+    public static void task2(int numberOfTheTeamMembers){
+        System.out.println("There are "+numberOfTheTeamMembers+ " in our team");
+        System.out.println("Lets meet our team members");
+    }
+    public static void task2(String teamMember){
+        System.out.println("One of the team member is "+ teamMember);
+    }
+    public static void task2(String teamMember2,String teamMember3){
+        System.out.println("The other members of our team are "+teamMember2+" , and "+teamMember3);
+        System.out.println("It is a pleasure to work together");
+    }
+
+}
