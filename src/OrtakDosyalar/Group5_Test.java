9c1c8506318c4b770b4a4e007b4f1076e43c8001
e1982b <e1982b@gmail.com>
2023-02-28 11:16:03 +0100

    Test Files again Uploaded

--- src/OrtakDosyalar/Group5_Test.java
+++ src/OrtakDosyalar/Group5_Test.java
@@ -2,6 +2,8 @@ package OrtakDosyalar;
 
 public class Group5_Test {
     public static void main(String[] args) {
-        System.out.println("Hallo");
+        System.out.println("Hello World");
+        System.out.println("Merhaba");
+        System.out.println("bla ");
     }
 }
