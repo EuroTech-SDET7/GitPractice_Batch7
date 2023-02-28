5f5bc3bad77931d1f62a931945a1a99b90a7f422
Bayram Bakir <bayrambakir46@gmail.com>
2023-02-28 15:51:44 +0100

    28.02.2023Bayram

--- src/GrupCalismasi/Team4.java
+++ src/GrupCalismasi/Team4.java
@@ -1,4 +1,10 @@
 package GrupCalismasi;
 
 public class Team4 {
+    public static void main(String[] args) {
+        System.out.println( "bayram batch 7 projesini homework testi gerceklestiriyor");
+        //yapilan islemlerin test edilmesi gerekiyor
+        //Yeni bir problem cozulmesi gerekiyor
+
+    }
 }
