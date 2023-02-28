07224dbfcd85205340240171aefa0349196e8109
ilhanTabak <herr.ilhantabak@gmail.com>
2023-02-28 15:01:34 +0100

    first step

--- src/GrupCalismasi/Team1.java
+++ src/GrupCalismasi/Team1.java
@@ -1,4 +1,9 @@
 package GrupCalismasi;
 
 public class Team1 {
+    public static void main(String[] args) {
+        System.out.println("team1");
+        System.out.println("grup calismasi");
+        System.out.println("sdet");
+    }
 }
