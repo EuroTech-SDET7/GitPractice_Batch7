dc6da51b713b16a1ba341cb1a86a93580e735159
Yusuf <ysfkrds.63@gmail.com>
2023-02-28 10:47:20 +0100

    confilikt sonrasi

--- src/OrtakDosyalar/Group5_Test.java
+++ src/OrtakDosyalar/Group5_Test.java
@@ -4,6 +4,8 @@ public class Group5_Test {
     public static void main(String[] args) {
         System.out.println("Merhaba");
         System.out.println("Hallo");
+        System.out.println(" alles paletti ");
+        System.out.println("whats up");
         //Burasi ortak dosyamiz
     }
 }
