1d3b2d11e5927ea14c5897e8fe8aaaf18b703083
Cetin Toprak <cetintoprak63@gmail.com>
2023-02-28 15:51:53 +0100

    Team4

--- src/GrupCalismasi/Team4.java
+++ src/GrupCalismasi/Team4.java
@@ -1,4 +1,15 @@
 package GrupCalismasi;
 
 public class Team4 {
+    public static void main(String[] args) {
+        // yorum satiri
+        System.out.println("GitHub");
+
+        int a = 25;
+        int b=30;
+
+        int c=a+b;
+        System.out.println("c = " + c);
+
+    }
 }
