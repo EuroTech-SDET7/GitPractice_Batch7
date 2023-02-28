efa8cb8d25155d312d5b797d5d7b51ab92d39a2a
Cemal <121253924+CemalOztel@users.noreply.github.com>
2023-02-28 15:30:14 +0100

    Update CemalNeu2.java
    sdsdsd

--- src/Bireysel/CemalNeu2.java
+++ src/Bireysel/CemalNeu2.java
@@ -3,5 +3,7 @@ package Bireysel;
 public class CemalNeu2 {
     public static void main(String[] args) {
         System.out.println("CemalNeu2" );
+       System.out.println("duzeltildi" );
+      
     }
 }
