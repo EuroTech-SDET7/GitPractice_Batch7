73aeb4af65748cdb4c2752c476e432eaefc258b9
Esra <esracaglar9067@gmail.com>
2023-02-28 14:26:03 +0100

    team eins grup uploaded

--- src/GrupCalismasi/Team1.java
+++ src/GrupCalismasi/Team1.java
@@ -1,4 +1,9 @@
 package GrupCalismasi;
 
 public class Team1 {
+    // esra selbst gemacht
+    public static void main(String[] args) {
+        System.out.println("test basariyla yapildi");
+
+    }
 }
