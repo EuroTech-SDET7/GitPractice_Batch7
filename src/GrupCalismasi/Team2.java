8d96a2a205d41841e12e6b03a075cc04d95ab920
Esref <csresrf@gmail.com>
2023-02-28 12:46:52 +0100

    team2 working conflict

--- src/GrupCalismasi/Team2.java
+++ src/GrupCalismasi/Team2.java
@@ -1,4 +1,9 @@
 package GrupCalismasi;
 
 public class Team2 {
+    public static void main(String[] args) {
+        System.out.println("team work");
+        //takım çalışması mı
+        //takım çatışması mı
+    }
 }
