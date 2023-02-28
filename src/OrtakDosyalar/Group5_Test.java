18ab0e72dc3e4a9593cf95b7993b62bce8b8b3c2
YahyaUygur <av.yahyauygur@gmail.com>
2023-02-28 10:38:22 +0100

    Base Branced setup

--- src/OrtakDosyalar/Group5_Test.java
+++ src/OrtakDosyalar/Group5_Test.java
@@ -3,5 +3,13 @@ package OrtakDosyalar;
 public class Group5_Test {
     public static void main(String[] args) {
         System.out.println("Hallo");
+
+        System.out.println(
+                "Hello"
+        );
+
+        int x=10;
+        int y=5;
     }
+
 }
