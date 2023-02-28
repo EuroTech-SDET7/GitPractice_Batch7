fde0e74b637e22186324276c6f5a2bbfa5cd9d2a
CemalOztel <cemaloztel6116@gmail.com>
2023-02-28 14:25:03 +0100

    Cemal Upload

---
+++ src/Bireysel/CemalNeu2.java
@@ -0,0 +1,7 @@
+package Bireysel;
+
+public class CemalNeu2 {
+    public static void main(String[] args) {
+        System.out.println("CemalNeu2" );
+    }
+}
--- src/GrupCalismasi/Team1.java
+++ src/GrupCalismasi/Team1.java
@@ -1,4 +1,8 @@
 package GrupCalismasi;
 
 public class Team1 {
+    public static void main(String[] args) {
+        //asdfghjkmnbv
+        System.out.println("Legenden sterben nie");
+    }
 }
